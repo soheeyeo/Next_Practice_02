1a255a934f72eb3dbd0ec880fbe68a1895008f80
soheeyeo <sohee407@gmail.com>
2023-08-07 14:57:38 +0900

    Dynamic route

--- app/list/page.js
+++ app/list/page.js
@@ -1,10 +1,10 @@
 import { connectDB } from "@/util/database"
+import Link from "next/link"
 
 export default async function List() {
 
     const db = (await connectDB).db("forum")
     let result = await db.collection('post').find().toArray()
-    console.log(result[0].title)
 
     return (
         <div className="list-bg">
@@ -12,8 +12,8 @@ export default async function List() {
             result.map((a, i) => {
                 return (
                     <div className="list-item" key={i}>
-                        <h4>{a.title}</h4>
-                        <p>{a.content}</p>
+                        <Link href={'/detail/' + result[i]._id}><h4>{a.title}</h4></Link>
+                        <p>1월 1일</p>
                     </div>
                 )
             })
