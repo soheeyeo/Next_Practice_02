0c23a3b84d019b74dcb551195743d3ea7e3a19ac
soheeyeo <sohee407@gmail.com>
2023-08-14 11:15:22 +0900

    cache

--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@ import { connectDB } from "@/util/database"
 
 export default async function Home() {
 
-  const db = (await connectDB).db("forum")
-  let result = await db.collection('post').find().toArray()
-  console.log(result)
+  // const db = (await connectDB).db("forum")
+  // let result = await db.collection('post').find().toArray()
+  // console.log(result)
+
+  await fetch('/URL', {cache : 'force-cache'})
 
   return (
     <div>안녕</div>
