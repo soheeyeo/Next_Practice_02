e1eed7fc398fcae5ce59d984a8e7ed3deee0e27e
soheeyeo <sohee407@gmail.com>
2023-08-14 11:18:32 +0900

    cache 02

---
+++ app/list2/ListItem.js
@@ -0,0 +1,40 @@
+'use client'
+
+import Link from "next/link"
+
+export default function ListItem({result}) {
+
+    return (
+        <div>
+            {
+                result.map((a, i) => {
+                    return (
+                        <div className="list-item" key={i}>
+                            <Link href={'/detail/' + result[i]._id}>
+                                <h4>{a.title}</h4>
+                            </Link>
+                            <Link href={'/edit/' + result[i]._id}>✏️</Link>
+                            <span onClick={(e)=>{
+                                // fetch('/api/post/delete', {
+                                //     method : 'DELETE', 
+                                //     body : result[i]._id
+                                // })
+                                // .then((r)=>{
+                                //     return r.json()
+                                // })
+                                // .then(()=>{
+                                //     e.target.parentElement.style.opacity = 0;
+                                //     setTimeout(() => {
+                                //         e.target.parentElement.style.display = 'none';
+                                //     }, 1000)
+                                // })
+                                fetch('/api/abc/yeo')
+                            }}>🗑️</span>
+                            <p>1월 1일</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import { connectDB } from "@/util/database"
 
+export const revalidate = 60;
+
 export default async function Home() {
 
-  // const db = (await connectDB).db("forum")
-  // let result = await db.collection('post').find().toArray()
-  // console.log(result)
-
-  await fetch('/URL', {cache : 'force-cache'})
+  const db = (await connectDB).db("forum")
+  let result = await db.collection('post').find().toArray()
+  console.log(result)
 
   return (
     <div>안녕</div>
