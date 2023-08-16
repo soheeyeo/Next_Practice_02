e4d60d3fc6c2255054a324ace54326e9547147a1
soheeyeo <sohee407@gmail.com>
2023-08-16 11:10:22 +0900

    JWT 02

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -3,10 +3,19 @@ import bcrypt from 'bcrypt'
 
 export default async function handler(요청, 응답) {
     if(요청.method == 'POST') {
-        let hash = await bcrypt.hash(요청.body.password, 10)
-        요청.body.password = hash
-        let db = (await connectDB).db('forum');
-        await db.collection('user_cred').insertOne(요청.body)
-        응답.status(200).json('가입성공')
+        if(요청.body.password == '') {
+            응답.status(500).json('비밀번호를 작성해주세요.')
+        } else  {
+            let hash = await bcrypt.hash(요청.body.password, 10)
+            요청.body.password = hash
+        }
+
+        if(요청.body.name == '' || 요청.body.email == '') {
+            응답.status(500).json('모두 작성해주세요.')
+        } else  {
+            let db = (await connectDB).db('forum');
+            await db.collection('user_cred').insertOne(요청.body)
+            응답.status(200).json('가입성공')
+        }
     }
 }
