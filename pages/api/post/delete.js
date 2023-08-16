5eefffc3564b65f68b768693459085df4babc41b
soheeyeo <sohee407@gmail.com>
2023-08-16 10:55:10 +0900

    JWT 01

--- pages/api/post/delete.js
+++ pages/api/post/delete.js
@@ -6,14 +6,13 @@ import { authOptions } from "../auth/[...nextauth]"
 export default async function handler(요청, 응답) {
     if(요청.method == 'DELETE') {
         let session = await getServerSession(요청, 응답, authOptions)
-        // console.log(요청.body)
+        console.log(요청.body)
 
         const db = (await connectDB).db('forum')
         let find = await db.collection('post').findOne({ _id : new ObjectId(요청.body) })
         
         if(find.author === session.user.email) {
             let result = await db.collection('post').deleteOne({ _id : new ObjectId(요청.body) })
-            // console.log(result)
             응답.status(200).json('삭제완료')
         } else  {
             응답.status(500).json('권한이 없습니다.')
