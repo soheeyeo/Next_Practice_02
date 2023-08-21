f0f3f06b7771b7d73a1e9e89fdba442594b3eaee
soheeyeo <sohee407@gmail.com>
2023-08-21 11:11:13 +0900

    localStorage

--- app/LoginBtn.js
+++ app/LoginBtn.js
@@ -1,8 +1,16 @@
 'use client'
 
 import { signIn } from 'next-auth/react'
+import { useEffect } from 'react'
 
 export default function LoginBtn() {
+
+    useEffect(()=>{
+        if(typeof window != 'undefined') {
+            localStorage.getItem('모드')
+        }
+    }, [])
+    
     return (
         <button onClick={()=>{ signIn() }}>로그인</button>
     )
