68ec156df535a77fc93dcd00e10dce11b10a028a
soheeyeo <sohee407@gmail.com>
2023-08-23 11:34:21 +0900

    Middleware 03

--- middleware.js
+++ middleware.js
@@ -1,8 +1,17 @@
-import { NextResponse } from 'next/server'
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
-    
-    if(request.nextUrl.pathname.startWith('/list')) {
+
+    const session = await getToken({req : request})
+    console.log(session)
+    if(request.nextUrl.pathname.startsWith('/write')){
+        if(session == null) {
+            return NextResponse.redirect(new URL('http://localhost:3000/api/auth/signin'), request.url)
+        }
+    }
+
+    if(request.nextUrl.pathname.startsWith('/list')) {
         console.log(new Date())
         console.log(request.headers.get('sec-ch-ua-platform'))
         return NextResponse.next()
